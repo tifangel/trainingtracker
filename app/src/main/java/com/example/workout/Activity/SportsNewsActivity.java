d42db01a23b9926c94d2f5ea664f9501ff8fd66e
Rehan Adi Satrya <13518061@std.stei.itb.ac.id>
2021-04-30 13:43:54 +0700

    update database

--- app/src/main/java/com/example/workout/Activity/SportsNewsActivity.java
+++ app/src/main/java/com/example/workout/Activity/SportsNewsActivity.java
@@ -82,8 +82,8 @@ public class SportsNewsActivity extends AppCompatActivity {
     private void pasangAdapter(List<News> newsList){
         recyclerNews = (RecyclerView) findViewById(R.id.recyclerNews);
         adapter = new SportsNewsAdapter(newsList);
-//        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(SportsNewsActivity.this, 3);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(SportsNewsActivity.this);
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(SportsNewsActivity.this, 3);
+//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(SportsNewsActivity.this);
         recyclerNews.setLayoutManager(layoutManager);
         recyclerNews.setAdapter(adapter);
         recyclerNews.addOnItemTouchListener(
