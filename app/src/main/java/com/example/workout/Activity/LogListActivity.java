2b0e39c720ff909f5b79c54139b335b3f34dc6f7
Rehan Adi Satrya <13518061@std.stei.itb.ac.id>
2021-04-30 21:06:37 +0700

    readme dan benerin db

--- app/src/main/java/com/example/workout/Activity/LogListActivity.java
+++ app/src/main/java/com/example/workout/Activity/LogListActivity.java
@@ -38,7 +38,7 @@ public class LogListActivity extends AppCompatActivity {
         tanggal = findViewById(R.id.tv_tanggalTerpilih);
         tanggal.setText(date);
 
-        fetch_history_workout();
+        fetch_history_workout(date);
 
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "workout-record").build();
@@ -67,9 +67,10 @@ public class LogListActivity extends AppCompatActivity {
         );
     }
 
-    private void fetch_history_workout() {
+    private void fetch_history_workout(String tanggal) {
         historyList = new ArrayList<>();
         historyList = AppDatabase.getDatabase(getApplicationContext()).getDao().getAll();
+        List<WorkoutRecord> tanggalDummy = AppDatabase.getDatabase(getApplicationContext()).getDao().findByDate(tanggal);
         historyList.add(new WorkoutRecord(1, "Walking", 50.0, 3000, date));
         historyList.add(new WorkoutRecord(2, "Running", 50.0, 3000, date));
         historyList.add(new WorkoutRecord(3, "Cycling", 50.0, 3000, date));
